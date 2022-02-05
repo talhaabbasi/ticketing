b653fd3b66e1050d726cb0dfb2c9c733ef96dc4f
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-05 13:37:21 +0500

    [FIX] Correct spelling

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,7 +3,7 @@ import "express-async-errors"
 import { json } from "body-parser"
 import cookieSession from "cookie-session"
 
-import { currentUserRouter } from "./routes/curent-user"
+import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from "./routes/signin"
 import { signoutRouter } from "./routes/signout"
 import { signupRouter } from "./routes/signup"
