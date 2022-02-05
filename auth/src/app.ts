5d8eda04caae032e366bea41a8fdfa3b9805a351
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-05 13:36:38 +0500

    [FEAT] Add signup test

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -13,14 +13,19 @@ import { NotFoundError } from "./errors/not-found-error"
 const app = express()
 app.set("trust proxy", true)
 app.use(json())
-app.use(cookieSession({ signed: false, secure: true }))
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all("*", async () => {
+app.all("*", async (req, res) => {
   throw new NotFoundError()
 })
 
