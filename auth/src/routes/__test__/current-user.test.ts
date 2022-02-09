246ce982c4420fba67a0059f47d58a3cab209802
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-09 21:58:00 +0500

    [FEAT] Add global authentication cookie

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,8 @@ import request from "supertest"
 import { app } from "../../app"
 
 it("responds with details about the current user", async () => {
-  const signupResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201)
+  const cookie = await global.getAuthCookie()
 
-  const cookie = signupResponse.get("Set-Cookie")
   const response = await request(app)
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
