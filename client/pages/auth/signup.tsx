84947d8ec370d7cc3b1d8a4168a71fedd52a858c
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-16 21:37:42 +0500

    [FEAT[ Add SignUp page

---
+++ client/pages/auth/signup.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next"
+
+const SignUp: NextPage = () => {
+  return (
+    <form>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+
+  )
+}
+
+export default SignUp
