bbe9c885f7222aa4931bd7da047a6eee39b3b873
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-23 21:31:04 +0500

    [FEAT] Handle Email and Password

--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,16 +1,24 @@
 import type { NextPage } from "next"
+import { useState } from "react"
 
 const SignUp: NextPage = () => {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  const onSubmit = (e: any) => {
+    e.preventDefault()
+
+  }
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input type="text" className="form-control" />
+        <input type="text" className="form-control" value={email} onChange={e => setEmail(e.target.value)}/>
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
