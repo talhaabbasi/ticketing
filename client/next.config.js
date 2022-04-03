ffab72abe21a62e5888b406979d88ffdec72f81f
talhaabbasi <talhaabbasi1997@gmail.com>
2022-04-03 22:41:29 +0500

    [FIX] Update client

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   webpackDevMiddleware: (config) => {
-    config.watchOptions.poll = 300
-    return config
+    config.watchOptions.poll = 300;
+    return config;
   },
-}
-
-module.exports = nextConfig
+};
