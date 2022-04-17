183415dfbc957893de9fe23592347cde3aeceae3
talhaabbasi <talhaabbasi1997@gmail.com>
2022-04-17 20:12:21 +0500

    [FIX] Update hosy

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,29 @@
-import axios from 'axios';
+import axios from "axios"
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
+  console.log(currentUser)
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
 
-  return <h1>Landing Page</h1>;
-};
+  return <h1>Landing Page</h1>
+}
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    const { data } = await axios.get(
+      "http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    )
+  } else {
+    const { data } = await axios.get("/api/users/currentuser")
+    return data
+  }
+  return {}
+}
 
-//   return response.data;
-// };
-
-export default LandingPage;
+export default LandingPage
